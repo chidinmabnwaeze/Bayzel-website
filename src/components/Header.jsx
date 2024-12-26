c2c77b874c02b2acb780e27520c080c6af91e059
chidinma nwaeze <chidinmabnwaeze@gmail.com>
2024-12-26 19:52:33 +0100

    merge

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,16 @@ import logo from "../assets/logo/logo bayzel.jpg";
 
 const Header = () => {
   return (
-    <header className="header flex p-4 fixed justify-between z-10" id="header">
+    <header className="header flex p-4 fixed justify-between z-10 bg-red-600" id="header">
       <div className="navbar-brand">
         <a className="bayzel" href="">
           <img className="logo" src={logo} alt="logo" />
           BAYZEL INTEGRATED SERVICES LIMITED
         </a>
       </div>
-      <nav className="navbar">
+      <nav className="navbar flex ">
         <div className="nav-menu">
-          <ul className="nav-lists">
+          <ul className="nav-lists flex">
             <li className="nav-list">
               <a className="nav-link text-2xl text-red-700" href="#home">
                 Home
